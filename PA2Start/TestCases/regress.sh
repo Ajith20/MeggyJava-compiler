e9f33f0226e8648c51b9080d341b1c7b814a2413
ajith20 <ajith0720@gmail.com>
2015-09-30 01:24:27 -0400

    PA2

--- PA2Start/TestCases/regress.sh
+++ PA2Start/TestCases/regress.sh
@@ -8,7 +8,7 @@ do
     echo "Regression testing MJPA2.jar $filename"
 
     # run the input file with the MJPA2 compiler
-    java -jar ../MJPA2.jar $filename  &> t.txt
+    java -jar ../MJPA2.jar $filename > t &
 
     diff t $filename.OK
     echo "DONE with MJPA2.jar $filename"
