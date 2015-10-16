e435ad9d37274d81b4b47277b7143dd012438282
ajith20 <ajith0720@gmail.com>
2015-10-16 16:38:50 -0400

    PA3 start

--- PA2Start/PA2Test1.java
+++ PA2Start/PA2Test1.java
@@ -5,9 +5,16 @@ import meggy.Meggy;
 	public static void main(String[] string1)
 	{
 		Meggy.setPixel((byte)1,(byte)2,Meggy.Color.RED);
-		//Meggy.setPixel((byte)1,(byte)2,Meggy.Color.BLUE);
+		Meggy.setPixel((byte)2,(byte)3,Meggy.Color.BLUE);
 		Meggy.setPixel((byte)3,(byte)4,Meggy.Color.BLUE);
 		Meggy.setPixel((byte)4,(byte)5,Meggy.Color.BLUE);
+		Meggy.setPixel((byte)5,(byte)6,Meggy.Color.RED);
+		Meggy.setPixel((byte)6,(byte)7,Meggy.Color.BLUE);
+		Meggy.setPixel((byte)7,(byte)1,Meggy.Color.BLUE);
+		Meggy.setPixel((byte)5,(byte)2,Meggy.Color.BLUE);
+		Meggy.setPixel((byte)6,(byte)3,Meggy.Color.RED);
+		Meggy.setPixel((byte)4,(byte)4,Meggy.Color.BLUE); 
+		
 	}
 }
 
--- PA2Start/PA2Test1.java.s
+++ PA2Start/PA2Test1.java.s
@@ -55,6 +55,40 @@ main:
  call   _Z12DisplaySlatev 
  
 # Load constant int num1 
+ ldi    r24,lo8(2) 
+ ldi    r25,hi8(2) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Casting num1 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Load constant int num2 
+ ldi    r24,lo8(3) 
+ ldi    r25,hi8(3) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Cast num2 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Color expression Meggy.Color.X 
+ ldi    r22,5 
+ # push one byte expression onto stack 
+ push   r22 
+ ### Meggy.setPixel(num1,num2,color) call 
+ # load a one byte expression off stack 
+ pop    r20 
+ # load a one byte expression off stack 
+ pop    r22 
+ # load a one byte expression off stack 
+ pop    r24 
+ call   _Z6DrawPxhhh 
+ call   _Z12DisplaySlatev 
+ 
+# Load constant int num1 
  ldi    r24,lo8(3) 
  ldi    r25,hi8(3) 
  # push two byte expression onto stack 
@@ -101,6 +135,210 @@ main:
  # Load constant int num2 
  ldi    r24,lo8(5) 
  ldi    r25,hi8(5) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Cast num2 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Color expression Meggy.Color.X 
+ ldi    r22,5 
+ # push one byte expression onto stack 
+ push   r22 
+ ### Meggy.setPixel(num1,num2,color) call 
+ # load a one byte expression off stack 
+ pop    r20 
+ # load a one byte expression off stack 
+ pop    r22 
+ # load a one byte expression off stack 
+ pop    r24 
+ call   _Z6DrawPxhhh 
+ call   _Z12DisplaySlatev 
+ 
+# Load constant int num1 
+ ldi    r24,lo8(5) 
+ ldi    r25,hi8(5) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Casting num1 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Load constant int num2 
+ ldi    r24,lo8(6) 
+ ldi    r25,hi8(6) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Cast num2 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Color expression Meggy.Color.X 
+ ldi    r22,1 
+ # push one byte expression onto stack 
+ push   r22 
+ ### Meggy.setPixel(num1,num2,color) call 
+ # load a one byte expression off stack 
+ pop    r20 
+ # load a one byte expression off stack 
+ pop    r22 
+ # load a one byte expression off stack 
+ pop    r24 
+ call   _Z6DrawPxhhh 
+ call   _Z12DisplaySlatev 
+ 
+# Load constant int num1 
+ ldi    r24,lo8(6) 
+ ldi    r25,hi8(6) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Casting num1 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Load constant int num2 
+ ldi    r24,lo8(7) 
+ ldi    r25,hi8(7) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Cast num2 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Color expression Meggy.Color.X 
+ ldi    r22,5 
+ # push one byte expression onto stack 
+ push   r22 
+ ### Meggy.setPixel(num1,num2,color) call 
+ # load a one byte expression off stack 
+ pop    r20 
+ # load a one byte expression off stack 
+ pop    r22 
+ # load a one byte expression off stack 
+ pop    r24 
+ call   _Z6DrawPxhhh 
+ call   _Z12DisplaySlatev 
+ 
+# Load constant int num1 
+ ldi    r24,lo8(7) 
+ ldi    r25,hi8(7) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Casting num1 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Load constant int num2 
+ ldi    r24,lo8(1) 
+ ldi    r25,hi8(1) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Cast num2 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Color expression Meggy.Color.X 
+ ldi    r22,5 
+ # push one byte expression onto stack 
+ push   r22 
+ ### Meggy.setPixel(num1,num2,color) call 
+ # load a one byte expression off stack 
+ pop    r20 
+ # load a one byte expression off stack 
+ pop    r22 
+ # load a one byte expression off stack 
+ pop    r24 
+ call   _Z6DrawPxhhh 
+ call   _Z12DisplaySlatev 
+ 
+# Load constant int num1 
+ ldi    r24,lo8(5) 
+ ldi    r25,hi8(5) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Casting num1 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Load constant int num2 
+ ldi    r24,lo8(2) 
+ ldi    r25,hi8(2) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Cast num2 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Color expression Meggy.Color.X 
+ ldi    r22,5 
+ # push one byte expression onto stack 
+ push   r22 
+ ### Meggy.setPixel(num1,num2,color) call 
+ # load a one byte expression off stack 
+ pop    r20 
+ # load a one byte expression off stack 
+ pop    r22 
+ # load a one byte expression off stack 
+ pop    r24 
+ call   _Z6DrawPxhhh 
+ call   _Z12DisplaySlatev 
+ 
+# Load constant int num1 
+ ldi    r24,lo8(6) 
+ ldi    r25,hi8(6) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Casting num1 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Load constant int num2 
+ ldi    r24,lo8(3) 
+ ldi    r25,hi8(3) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Cast num2 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Color expression Meggy.Color.X 
+ ldi    r22,1 
+ # push one byte expression onto stack 
+ push   r22 
+ ### Meggy.setPixel(num1,num2,color) call 
+ # load a one byte expression off stack 
+ pop    r20 
+ # load a one byte expression off stack 
+ pop    r22 
+ # load a one byte expression off stack 
+ pop    r24 
+ call   _Z6DrawPxhhh 
+ call   _Z12DisplaySlatev 
+ 
+# Load constant int num1 
+ ldi    r24,lo8(4) 
+ ldi    r25,hi8(4) 
+ # push two byte expression onto stack 
+ push   r25 
+ push   r24 
+ # Casting num1 to byte 
+ pop    r24 
+ pop    r25 
+ push   r24 
+ # Load constant int num2 
+ ldi    r24,lo8(4) 
+ ldi    r25,hi8(4) 
  # push two byte expression onto stack 
  push   r25 
  push   r24 
