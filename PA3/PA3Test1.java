6a1765170a28fb39576bd64af20ddff7038103a0
ajith20 <ajith0720@gmail.com>
2015-10-20 17:05:28 -0400

    AST working tested - completed

--- PA3/PA3Test1.java
+++ PA3/PA3Test1.java
@@ -1,9 +1,15 @@
 import meggy.Meggy;
 
- class PA3Test1
+class PA3Test1
 {
 	public static void main(String[] string1)
 	{
-		
+		Meggy.setPixel((byte)1,(byte)2,Meggy.Color.BLUE);
+		Meggy.delay(500);
+		if (Meggy.checkButton(Meggy.Button.Up) && (Meggy.getPixel((byte)1,(byte)2) == Meggy.Color.BLUE))
+		{
+			Meggy.setPixel((byte)(1+5),(byte)3,Meggy.Color.WHITE);
+			Meggy.setPixel((byte)(2-1),(byte)3,Meggy.Color.WHITE);
+		}
 	}
 }
